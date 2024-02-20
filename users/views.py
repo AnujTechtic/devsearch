d115608fea3cf4f3dc8f7360eb5fe8ac4f13e558
AnujTechtic <158455565+AnujTechtic@users.noreply.github.com>
2024-02-20 10:17:34 +0530

    Sevetenth Video

--- users/views.py
+++ users/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+
 from .models import Profile
 
 # Create your views here.
+def loginPage(request):
+ if request.method == 'POST':
+  username = request.POST['username']
+  password = request.POST['password']
+  try:
+       user = User.objects.get(username = username)
+  except:
+        print("Username does not exist")
+
+  user = authenticate(request, username=username, password=password)
+
+  if user is not None:
+     login(request, user )
+     return redirect('profiles')
+  else:
+     print("Username or Password is Incorrect")
+
+
+ return render(request, 'users/login-register.html')
+
+
 def profiles(request):
  profiles = Profile.objects.all()
  context = {'profiles': profiles}
