799521312123db4b5420351c898e3d3534f848fe
AnujTechTic <anuj.s@techtic.agency>
2024-03-07 17:31:52 +0530

    reviews

--- projects/views.py
+++ projects/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Project, Tag
 from .forms import ProjectForm, ReviewForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.db.models import Q
 from .utils import searchProjects, paginateProjects
 
@@ -19,6 +20,19 @@ def projecs(request):
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     form = ReviewForm()
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+
+        projectObj.getVoteCount
+
+        messages.success(request, "Your review was successfully submitted!")
+        return redirect('project', pk = projectObj.id)
+
+
     return render(request, 'projects/project.html', {'project':projectObj, 'form':form})
 
 @login_required(login_url='login')
